33b22e21f07fc245dfe8ae6b886b60efb900b1d7
atishayjain <atishay.jain@nagarro.com>
2023-01-05 22:09:36 +0530

    news api updated

--- src/main/java/com/atishay/newsapp/NewsappApplication.java
+++ src/main/java/com/atishay/newsapp/NewsappApplication.java
@@ -6,17 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class NewsappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsappApplication.class, args);
 	}
 	
-	@GetMapping("/")
-	public String home() {
-		// TODO Auto-generated method stub
-		return "working hurray";
-	}
+	
 
 }
